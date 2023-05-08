9e419db48e281a84c8e7ccd7a5e657edc07a82b8
monir3355 <16173103036@cse.bubt.edu.bd>
2023-05-09 02:00:03 +0600

    user deleted coffee from database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,6 +39,14 @@ async function run() {
       const result = await coffeeCollection.insertOne(coffee);
       res.send(result);
     });
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
